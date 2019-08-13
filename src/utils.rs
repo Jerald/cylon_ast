e6c57d895653fffcea333520dd7de9bd17c8faee
Oscar <osmithsi@sfu.ca>
2019-08-13 10:16:08 -0700

    Modified from macro and added try_from macro

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 #[macro_export]
-macro_rules! boxed_conversion_impl {
+macro_rules! boxed_from_impl {
     ( $(From<Box<$t:ty>> for Box<$g:ty>);+ ) => {
         $(
             impl From<Box<$t>> for Box<$g>
@@ -9,6 +9,25 @@ macro_rules! boxed_conversion_impl {
                 fn from(boxed: Box<$t>) -> Box<$g>
                 {
                     Box::new((*boxed).into())
+                }
+            }
+        )+
+    };
+}
+
+#[macro_export]
+macro_rules! boxed_try_from_impl {
+    ( $(TryFrom<Box<$t:ty>> for Box<$g:ty>);+ ) => {
+        $(
+            impl TryFrom<Box<$t>> for Box<$g>
+            where
+                $t: TryInto<$g>
+            {
+                type Error = <$t as TryFrom>::Error;
+
+                fn try_from(boxed: Box<$t>) -> Result<Box<$g>, Self::Error>
+                {
+                    Ok(Box::new((*boxed).try_into()?))
                 }
             }
         )+
